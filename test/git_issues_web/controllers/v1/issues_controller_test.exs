1b786c8b343981a9beaaf8d738fc859cbf70d333
Vinicius Silveira <viniciussilveira6@gmail.com>
2024-02-15 19:13:06 -0400

    feat(app): GI-1 cria endpoint para receber input de repositórios
    Cria o endpoint  para receber input de um repositório a ser coletado. A coleta ou qualquer outra lógica nào foi implementada o endpoint apenas recebe a request e retorna o status 204

--- test/git_issues_web/controllers/v1/issues_controller_test.exs
+++ test/git_issues_web/controllers/v1/issues_controller_test.exs
@@ -3,7 +3,7 @@ defmodule GitIssues.V1.IssuesControllerTest do
 
   describe "GET /git-issues/api/v1/issues?username=&repo=" do
     test " with success", %{conn: conn} do
-      conn = get(conn, "/git-issues/api/issues?username=elixir-lang&repo=elixir")
+      conn = get(conn, "/git-issues/api/v1/issues?username=elixir-lang&repo=elixir")
 
       assert conn.status == 204
     end
