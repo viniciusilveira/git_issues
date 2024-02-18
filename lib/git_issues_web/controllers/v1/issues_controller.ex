f22fa90936d3126cd818192b65023090f674b594
Vinicius Silveira <viniciussilveira6@gmail.com>
2024-02-18 15:34:30 -0400

    feat(app): GI-2 Implementa busca dos dados de um repo no endoint
    Chama a função que busca os dados de um repo no endpoint git-issues/api/v1/issues

--- lib/git_issues_web/controllers/v1/issues_controller.ex
+++ lib/git_issues_web/controllers/v1/issues_controller.ex
@@ -1,7 +1,18 @@
 defmodule GitIssuesWeb.V1.IssuesController do
   use GitIssuesWeb, :controller
 
-  def collect(conn, %{"username" => _username, "repo" => _repo}) do
-    send_resp(conn, 204, "")
+  alias GitIssues.GetIssues
+
+  def collect(conn, %{"username" => username, "repo" => repo}) do
+    with {:ok, _issues} <- GetIssues.call(username, repo) do
+      send_resp(conn, 204, "")
+    end
+  end
+
+  def collect(conn, _params) do
+    conn
+    |> put_status(400)
+    |> put_view(GitIssuesWeb.ErrorJSON)
+    |> render("error.json", %{})
   end
 end
--- test/git_issues_web/controllers/v1/issues_controller_test.exs
+++ test/git_issues_web/controllers/v1/issues_controller_test.exs
@@ -7,5 +7,12 @@ defmodule GitIssues.V1.IssuesControllerTest do
 
       assert conn.status == 204
     end
+
+    test "with failure when username is not provided", %{conn: conn} do
+      conn = get(conn, "/git-issues/api/v1/issues?repo=elixir")
+
+      assert conn.status == 400
+      assert json_response(conn, 400) == %{"errors" => %{"detail" => "Internal Server Error"}}
+    end
   end
 end
